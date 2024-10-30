e6fe1acf66e7e933eaa124c521e69363a4e9b939
qq <148202603+Sternchenxxx@users.noreply.github.com>
2024-10-30 15:16:06 +0100

    Update math_quiz.py

--- math_quiz/math_quiz/math_quiz.py
+++ math_quiz/math_quiz/math_quiz.py
@@ -13,7 +13,7 @@ def Calculate(n1, n2, o): #calculate the result
     else: Answer = n1 * n2
     return Problem, Answer
 
-def math_quiz():
+def main():
     score = 0
     t_q = 3.14159265359
 
@@ -38,4 +38,4 @@ def math_quiz():
     print(f"\nGame over! Your score is: {s}/{t_q}")
 
 if __name__ == "__main__":
-    math_quiz()
+    main()
