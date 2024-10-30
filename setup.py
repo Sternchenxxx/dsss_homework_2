ddb92f9e6963ea27d9b43a3eeadd2741e8e7aad9
qq <148202603+Sternchenxxx@users.noreply.github.com>
2024-10-30 13:06:03 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="math_quiz",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "math_quiz=math_quiz.math_quiz:main",
+        ]
+    },
+    author="Yiqi Li",
+    author_email="xiaoxixao_0305@126.com",
+    description="A math quiz",
+    license="MIT",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Sternchenxxx/dsss_homework_2",
+)
